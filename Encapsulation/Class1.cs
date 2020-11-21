fd92d94a8784e59cd84c5c3bf44b7e1ac0094dd8
KRISHNA\Krishna Teja <ladsai1394@gmail.com>
2020-11-21 16:30:35 +0530

    first commit

---
+++ Encapsulation/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    public class Class1
+    {
+        public string[] c = { "vishal", "Krish", "vinay" };
+        public int[] i = { 1, 2, 3, };
+    }
+}
